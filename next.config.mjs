d669d08c19ead51b8c43b7049c5f87590d2dc7cb
marindg <marindanger@gmail.com>
2024-02-13 15:14:16 +0100

    add sidebar

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      }, // {protocol: "https", host: "images.unsplash.com", path: "/.*"
+    ],
+  },
+};
 
 export default nextConfig;
