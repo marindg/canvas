5d9a4e4b022820c5d5b1982428a8f1b389c4dbc9
marindg <marindanger@gmail.com>
2024-02-20 07:40:49 +0100

    selection net

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import {
   Camera,
   Color,
+  Layer,
   Point,
   Side,
   XYWH,
@@ -81,4 +82,41 @@ export function resizeBounds(
   }
 
   return result;
+}
+
+export function findIntersectingLayersWithRectangle(
+  layerIds: readonly string[],
+  layers: ReadonlyMap<string, Layer>,
+  a: Point,
+  b: Point
+) {
+  const rect = {
+    x: Math.min(a.x, b.x),
+    y: Math.min(a.y, b.y),
+    width: Math.abs(a.x - b.x),
+    height: Math.abs(a.y - b.y),
+  };
+
+  const ids = [];
+
+  for (const layerId of layerIds) {
+    const layer = layers.get(layerId);
+
+    if (layer == null) {
+      continue;
+    }
+
+    const { x, y, height, width } = layer;
+
+    if (
+      rect.x + rect.width > x &&
+      rect.x < x + width &&
+      rect.y + rect.height > y &&
+      rect.y < y + height
+    ) {
+      ids.push(layerId);
+    }
+  }
+
+  return ids;
 }
